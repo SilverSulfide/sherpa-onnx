047db799d2fbe8b8364932465c7569c63ea05ae0
martins.kuznecovs <martins.kuznecovs@gmail.com>
2024-07-11 16:04:39 +0300

    WIP display LM probs

--- sherpa-onnx/csrc/online-transducer-modified-beam-search-decoder.cc
+++ sherpa-onnx/csrc/online-transducer-modified-beam-search-decoder.cc
@@ -239,6 +239,8 @@ void OnlineTransducerModifiedBeamSearchDecoder::Decode(
     auto &r = (*result)[b];
 
     r.hyps = std::move(hyps);
+    // display lm probs on result
+    r.lm_probs = std::move(best_hyp.lm_probs)
     r.tokens = std::move(best_hyp.ys);
     r.num_trailing_blanks = best_hyp.num_trailing_blanks;
     r.frame_offset += num_frames;
